cbdf8ce86f5981ec3ad57b7bb8a6a7e372b41bb5
yangmaoting <524250484@qq.com>
2023-08-16 17:24:23 +0800

    feat-增加探探卡片-增加排列

---
+++ app/src/main/java/com/sundayting/wancompose/page/examplewidget/tantancard/TestExample.kt
@@ -0,0 +1,128 @@
+package com.sundayting.wancompose.page.examplewidget.tantancard
+
+import com.sundayting.wancompose.R
+
+object TestExample {
+
+    val userList = listOf(
+        TanTanUserBean(
+            name = "等待一个人",
+            picList = listOf(
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/7613df5dd2094881bdf2b83115e3b3c3.jpeg",
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/94127d0f67da450e98e6f669070ad69b.jpeg",
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/881e9a9b620e44698aa4d64a8d756088.jpeg",
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/edf7266c067644c2a43346b7155703a7.jpeg"
+            ),
+            basicDetail = TanTanUserBean.BasicDetail(
+                isMale = false,
+                age = 14,
+                tagList = listOf(
+                    TanTanUserBean.BasicDetail.Tag(
+                        icon = R.drawable.ic_taxi,
+                        content = "可外出"
+                    ),
+                    TanTanUserBean.BasicDetail.Tag(
+                        icon = R.drawable.ic_find_more,
+                        content = "发现更多"
+                    ),
+                ),
+                location = "广州黄埔（10km）·11分钟前活跃"
+            ),
+            recentPost = TanTanUserBean.RecentPost(
+                picList = listOf(
+                    "https://wx3.sinaimg.cn/mw690/001WN8zPly8hgvfjc0cxhj60j60cs41802.jpg",
+                    "https://wx3.sinaimg.cn/mw690/001WN8zPly8hgvfjc6sxwj60j60de0vx02.jpg",
+                    "https://wx4.sinaimg.cn/mw690/001WN8zPly8hgvfjciosfj60j60csgmv02.jpg"
+                )
+            )
+        ),
+        TanTanUserBean(
+            name = "晓屿_拾念",
+            picList = listOf(
+                "https://wx4.sinaimg.cn/mw690/e6470c42ly1hgpggyfvtgj22802you0y.jpg",
+                "https://wx2.sinaimg.cn/mw690/e6470c42ly1hgpggac3mqj22702you0z.jpg",
+                "https://picx.zhimg.com/80/v2-9c8e2b5642da4e5e76b0f33070df3760_1440w.webp?source=1940ef5c"
+            ),
+            basicDetail = TanTanUserBean.BasicDetail(
+                isMale = false,
+                age = 14,
+                tagList = listOf(
+                    TanTanUserBean.BasicDetail.Tag(
+                        icon = R.drawable.ic_find_more,
+                        content = "在找同伴"
+                    ),
+                    TanTanUserBean.BasicDetail.Tag(
+                        content = "发现更多"
+                    ),
+                ),
+                location = "广州天河（30km）·30分钟前活跃"
+            ),
+            recentPost = TanTanUserBean.RecentPost(
+                picList = listOf(
+                    "https://wx2.sinaimg.cn/orj360/401e1dffly1hgqpcob1tjj20zk1bfgsg.jpg",
+                    "https://wx4.sinaimg.cn/orj360/401e1dffly1hgqpcoqknjj211a1dpnb5.jpg",
+                    "https://pic1.zhimg.com/80/v2-c891532c8663f33a1168c833c67f6c26_1440w.webp?source=1940ef5c"
+                )
+            )
+        ),
+        TanTanUserBean(
+            name = "鸡你太美",
+            picList = listOf(
+                "https://picx.zhimg.com/80/v2-9c8e2b5642da4e5e76b0f33070df3760_1440w.webp?source=1940ef5c"
+            ),
+            basicDetail = TanTanUserBean.BasicDetail(
+                isMale = false,
+                age = 14,
+                tagList = listOf(
+                    TanTanUserBean.BasicDetail.Tag(
+                        icon = R.drawable.ic_find_more,
+                        content = "在找同伴"
+                    ),
+                    TanTanUserBean.BasicDetail.Tag(
+                        content = "发现更多"
+                    ),
+                ),
+                location = "广州天河（30km）·30分钟前活跃"
+            ),
+            recentPost = TanTanUserBean.RecentPost(
+                picList = listOf(
+                    "https://wx2.sinaimg.cn/orj360/401e1dffly1hgqpcob1tjj20zk1bfgsg.jpg",
+                    "https://wx4.sinaimg.cn/orj360/401e1dffly1hgqpcoqknjj211a1dpnb5.jpg",
+                    "https://pic1.zhimg.com/80/v2-c891532c8663f33a1168c833c67f6c26_1440w.webp?source=1940ef5c"
+                )
+            )
+        ),
+        TanTanUserBean(
+            name = "等待一个人",
+            picList = listOf(
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/7613df5dd2094881bdf2b83115e3b3c3.jpeg",
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/94127d0f67da450e98e6f669070ad69b.jpeg",
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/881e9a9b620e44698aa4d64a8d756088.jpeg",
+                "https://5b0988e595225.cdn.sohucs.com/images/20190325/edf7266c067644c2a43346b7155703a7.jpeg"
+            ),
+            basicDetail = TanTanUserBean.BasicDetail(
+                isMale = false,
+                age = 14,
+                tagList = listOf(
+                    TanTanUserBean.BasicDetail.Tag(
+                        icon = R.drawable.ic_taxi,
+                        content = "可外出"
+                    ),
+                    TanTanUserBean.BasicDetail.Tag(
+                        icon = R.drawable.ic_find_more,
+                        content = "发现更多"
+                    ),
+                ),
+                location = "广州黄埔（10km）·11分钟前活跃"
+            ),
+            recentPost = TanTanUserBean.RecentPost(
+                picList = listOf(
+                    "https://wx3.sinaimg.cn/mw690/001WN8zPly8hgvfjc0cxhj60j60cs41802.jpg",
+                    "https://wx3.sinaimg.cn/mw690/001WN8zPly8hgvfjc6sxwj60j60de0vx02.jpg",
+                    "https://wx4.sinaimg.cn/mw690/001WN8zPly8hgvfjciosfj60j60csgmv02.jpg"
+                )
+            )
+        ),
+    )
+
+}
